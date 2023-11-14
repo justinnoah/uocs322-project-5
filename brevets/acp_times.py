c89c369991ddafeb6343e43da188e49b595cde4f
Justin Noah <justinnoah@gmail.com>
2023-11-13 19:40:28 -0800

    a lot of work not split into commits

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -5,17 +5,15 @@ following rules described at https://rusa.org/octime_acp.html
 and https://rusa.org/pages/rulesForRiders
 """
 import math
-from enum import Enum
 
 import arrow
-from flask import current_app as app
 
-race_type = {
-    200.0:  { 'dist': 200.0, 'min_speed': 15.0,   'max_speed': 34.0, 'max_time': 13.5 },
-    300.0:  { 'dist': 100.0, 'min_speed': 15.0,   'max_speed': 32.0, 'max_time': 20.0 },
-    400.0:  { 'dist': 200.0, 'min_speed': 15.0,   'max_speed': 32.0, 'max_time': 27.0 },
-    600.0:  { 'dist': 200.0, 'min_speed': 15.0,   'max_speed': 30.0, 'max_time': 40.0 },
-    1000.0: { 'dist': 400.0, 'min_speed': 11.428, 'max_speed': 28.0, 'max_time': 51.0 },
+brevet_controls = {
+    0:    { 'max_speed': 34.0 },
+    200:  { 'min_speed': 15.0,   'max_speed': 32.0, 'max_time': 13.5 },
+    400:  { 'min_speed': 15.0,   'max_speed': 30.0, 'max_time': 27.0 },
+    600:  { 'min_speed': 15.0,   'max_speed': 28.0, 'max_time': 40.0 },
+    1000: { 'min_speed': 11.428, 'max_speed': 26.0, 'max_time': 51.0 },
 }
 
 #  You MUST provide the following two functions
@@ -23,96 +21,6 @@ race_type = {
 #  these signatures even if you don't use all the
 #  same arguments.
 #
-class OC(Enum):
-    OPEN = 1
-    CLOSE = 2
-
-def shift_time(
-        control_dist_km: float,
-        brevet_dist_km: float,
-        brevet_start_time: arrow.arrow.Arrow,
-        oc: OC
-) -> arrow.arrow.Arrow:
-    brevet_start_time = arrow.get(brevet_start_time.format("YYYY-MM-DD HH:mm"))
-    # Clamp the lower bound for control and brevet distances at 0km
-    if control_dist_km < 0.:
-        control_dist_km = 0.
-    if brevet_dist_km < 0.:
-        brevet_dist_km = 0.
-
-    # Prep for open / close differences
-    minmax = None
-    if oc == OC.OPEN:
-        minmax = 'max'
-        if control_dist_km == 0.0:
-            return brevet_start_time
-    elif oc == OC.CLOSE:
-        minmax = 'min'
-        if control_dist_km == 0.0:
-            return brevet_start_time.shift(hours=+1)
-    speed = "%s_speed" % minmax
-    print("speed: %s" % speed)
-
-    # Clamp distance to 120%
-    print("distance: %s" % control_dist_km)
-    if control_dist_km > brevet_dist_km:
-        control_dist_km = brevet_dist_km
-    print("post clamp distance: %s" % control_dist_km)
-
-    current_brevet_range = race_type[brevet_dist_km]
-    print("Using brevet: %s - %s" % (brevet_dist_km, current_brevet_range))
-
-    # Make a sorted list of the distances we will be calculating with
-    distances = list(race_type.keys())
-    distances.sort()
-    print("distances to use in calcs: %s" % distances)
-
-    if brevet_dist_km == 200.:
-        distances = [distances[0]]
-
-    # Remove the ones we aren't using
-    while len(distances) >= 2:
-        if not (control_dist_km > distances[-2] and control_dist_km < distances[-1]):
-            # Throw the value away
-            distances.pop()
-        else:
-            break
-    print("Clamped distances: %s" % distances)
-
-    # Amount to shift start time by in hours
-    shift = 0.0
-    closed = False
-
-    while len(distances) > 0:
-        # The control segment to calculate from
-        control_dist = distances[-1]
-        segment = race_type[control_dist]
-
-        # Add to shift for each segment
-        if control_dist_km >= control_dist:
-            if oc == OC.OPEN:
-                if not closed:
-                    shift += math.ceil(segment['dist'] / segment[speed] * 60) / 60.
-            else:
-                if not closed:
-                    shift += segment['max_time']
-                    closed = True
-        else:
-            if control_dist >= 300.:
-                seg_dist = math.ceil(control_dist_km - distances[-2])
-                seg_time = math.ceil(seg_dist / race_type[distances[-2]][speed] * 60.) / 60.
-                shift += seg_time
-            else:
-                seg_dist = math.ceil(control_dist_km)
-                seg_time = math.ceil(seg_dist / race_type[200.][speed])
-                shift += seg_time
-        distances.pop()
-        print("shifting... %s" % shift)
-
-    print("shifting: %s hours..." % shift)
-    return brevet_start_time.shift(hours=+shift)
-
-
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
     Args:
@@ -125,7 +33,21 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return shift_time(control_dist_km, brevet_dist_km, brevet_start_time, OC.OPEN)
+    rounded_control_km = round(control_dist_km)
+    clamp_km = rounded_control_km if brevet_dist_km > rounded_control_km else brevet_dist_km
+    start_time = arrow.get(brevet_start_time)
+    hours_to_shift = 0
+
+    for k, v in sorted(brevet_controls.items(), reverse=False):
+        dist_over = clamp_km - k
+        if dist_over > 0:
+            clamp_km -= dist_over
+            hours_to_shift = dist_over / v["max_speed"] + (1 / 120) # + 30sec for rounding reasons
+
+    hours, minutes_float = math.modf(hours_to_shift)
+    minutes = round(minutes_float *  60)
+    return start_time.shift(hours=hours, minutes=minutes)
+
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -139,5 +61,26 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return shift_time(control_dist_km, brevet_dist_km, brevet_start_time, OC.CLOSE)
+    rounded_control_km = round(control_dist_km)
+    clamp_km = rounded_control_km if brevet_dist_km > rounded_control_km else brevet_dist_km
+    start_time = arrow.get(brevet_start_time)
+    hours_to_shift = 0
+
+    # timing rules by spec
+    if clamp_km <= 60:
+        hours_to_shift = 1
+        hours_to_shift += clamp_km / 20
+    elif clamp_km >= brevet_dist_km:
+        hours_to_shift = brevet_controls[brevet_dist_km]["max_time"]
+    else:
+        for k, v in brevet_controls.items():
+            if k == 0:
+                continue
+            hours_to_shift += (k if k < clamp_km else clamp_km) / v["min_speed"]
+
+    # Create hours and minutes from hours as float
+    hours, minutes_float = math.modf(hours_to_shift)
+    minutes = round(minutes_float *  60)
+    # Return brevet_start_time shifted by calculated time
+    return start_time.shift(hours=hours, minutes=minutes)
 
--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -4,13 +4,34 @@ Replacement for RUSA ACP brevet time calculator
 
 """
 
+import copy
+import logging
+
+import arrow
 import flask
 from flask import request
-import arrow  # Replacement for datetime, based on moment.js
+
 import acp_times  # Brevet time calculations
 import config
+from pymongo import MongoClient
+import pymongo
 
-import logging
+def init_db():
+    client = MongoClient("mongodb://db:27017/")
+    db = client["brevets"]
+
+    datum = {'km': 0, 'loc': "", 'row': 0}
+    data = []
+    for i in range(0, 20):
+        d = copy.deepcopy(datum)
+        d['row'] += i
+        data.append(d)
+    ws = {
+        "timestamp": arrow.utcnow().format(MGFMT),
+        "worksheet": data
+    }
+    db.worksheets.insert_one(ws)
+    return (client, db)
 
 ###
 # Globals
@@ -18,6 +39,23 @@ import logging
 app = flask.Flask(__name__)
 CONFIG = config.configuration()
 DTFMT = 'YYYY-MM-DDTHH:mm'
+MGFMT = f"{DTFMT}:ss"
+(client, db) = init_db()
+
+
+def insert_ws(worksheet):
+    ws = {
+        "timestamp": arrow.utcnow().format(MGFMT),
+        "worksheet": worksheet
+    }
+    db.worksheets.insert_one(ws)
+
+def latest_ws():
+    document = db.worksheets.find_one(sort=[("timestamp", pymongo.DESCENDING)])
+    if document:
+        return document["worksheet"]
+    else:
+        return None
 
 ###
 # Pages
@@ -51,7 +89,7 @@ def _calc_times():
     Expects one URL-encoded argument, the number of miles.
     """
     app.logger.debug("Got a JSON request")
-    km = request.args.get('km', 999, type=float)
+    km = request.args.get('km', 999., type=float)
     start_time = arrow.get(request.args.get('start_time', arrow.now().format(DTFMT), type=str), DTFMT)
     control_dist = request.args.get("control_dist", 200, type=int)
 
@@ -63,6 +101,22 @@ def _calc_times():
     close_time = acp_times.close_time(km, control_dist, start_time).format('YYYY-MM-DDTHH:mm')
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
+
+@app.route('/_save_worksheet', methods=["POST"])
+def store_worksheet():
+    data = app.json.loads(request.get_data())
+    app.logger.debug(f"Request Data: {data}")
+    insert_ws(data)
+    return "", 200
+
+@app.route('/_restore_worksheet', methods=["GET"])
+def send_worksheet():
+    latest = latest_ws()
+    app.logger.debug("data: %s" % latest)
+    if latest:
+        return flask.jsonify(worksheet=latest)
+    else:
+        return flask.jsonify(worksheet={})
 
 
 #############
